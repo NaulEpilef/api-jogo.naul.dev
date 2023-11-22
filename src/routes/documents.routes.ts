b391dcc2f7018a9ec1d9a8d49b565fbe83b1b801
Naul-Epilef <lfps1999@gmail.com>
2023-11-21 22:41:10 -0300

    documents/random route return only one document by time

--- src/routes/documents.routes.ts
+++ src/routes/documents.routes.ts
@@ -18,30 +18,27 @@ const generateCodes = () => {
 
 route.get("/random", (req, res) => {
 	const names = ["Davi", "Naul", "Elder", "Fan", "Jason"];
-	const d = [] as any[];
-	names.forEach(n => {
-		const docs: IDocuments = {
-			id: {
-				name: n,
-				cpf: generateCodes(),
-        dateBirth: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
-				dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
-			},
-			driverLicense: {
-				name: n,
-				cpf: generateCodes(),
-				dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
-			},
-			// workVisa: {
-			// 	name: n,
-			// 	cnpj: generateCodes(),
-			// 	dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
-			// }
-		};
-		d.push(docs);
-	});
+  const randomName = names[randomNumberInRange(0, names.length-1)];
+  const docs: IDocuments = {
+    id: {
+      name: randomName,
+      cpf: generateCodes(),
+      dateBirth: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
+      dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
+    },
+    driverLicense: {
+      name: randomName,
+      cpf: generateCodes(),
+      dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
+    },
+    // workVisa: {
+    // 	name: randomName,
+    // 	cnpj: generateCodes(),
+    // 	dateValidate: moment().locale("pt-br").add(randomNumberInRange(-6, 6), 'd'),
+    // }
+  };
 
-	res.status(200).json(d);
+	res.status(200).json(docs);
 });
 
 export default route;
